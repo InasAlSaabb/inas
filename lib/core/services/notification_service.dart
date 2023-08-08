fe57d32db7ac10094b03d04ff88252331e02ce30
InasAlSaab <sososaab618@gmail.com>
2023-08-08 09:21:40 +0300

    ...

--- lib/core/services/notification_service.dart
+++ lib/core/services/notification_service.dart
@@ -1,5 +1,5 @@
 // import 'dart:async';
-//
+
 // import 'package:firebase_messaging/firebase_messaging.dart';
 // import 'package:flutter_templete/core/data/models/notification_model.dart';
 // import 'package:flutter_templete/core/enums/app_state_enum.dart';
@@ -7,19 +7,19 @@
 // import 'package:flutter_templete/core/utils/general_utils.dart';
 // import 'package:flutter_templete/ui/shared/utils.dart';
 // import 'package:get/utils.dart';
-//
+
 // class NotificationService {
 //   StreamController<NotifictionModel> notifcationStream =
 //       StreamController<NotifictionModel>.broadcast();
-//
+
 //   NotificationService() {
 //     onInit();
 //   }
-//
+
 //   void onInit() async {
 //     if (GetPlatform.isIOS) {
 //       FirebaseMessaging messaging = FirebaseMessaging.instance;
-//
+
 //       NotificationSettings settings = await messaging.requestPermission(
 //         alert: true,
 //         announcement: false,
@@ -29,7 +29,7 @@
 //         provisional: false,
 //         sound: true,
 //       );
-//
+
 //       if (settings.authorizationStatus == AuthorizationStatus.authorized) {
 //         await registerdFCMToken();
 //       } else {
@@ -38,32 +38,32 @@
 //     } else {
 //       await registerdFCMToken();
 //     }
-//
+
 //     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
 //       NotifictionModel model = NotifictionModel.fromJson(message.data);
 //       handelNotification(model: model, appState: AppState.FOREGROUND);
 //     });
-//
+
 //     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
 //       NotifictionModel model = NotifictionModel.fromJson(message.data);
 //       handelNotification(model: model, appState: AppState.BACKGROUND);
 //     });
 //   }
-//
+
 //   Future<void> registerdFCMToken() async {
 //     final fcmToken = await FirebaseMessaging.instance.getToken();
 //     //! -- Call api that register fcm token ---
-//
+
 //     FirebaseMessaging.instance.onTokenRefresh.listen((fcmToken) {
 //       //! -- Call api that register fcm token ---
-//
+
 //       // Note: This callback is fired at each app startup and whenever a new
 //       // token is generated.
 //     }).onError((err) {
 //       // Error getting token.
 //     });
 //   }
-//
+
 //   void handelNotification(
 //       {required NotifictionModel model, required AppState appState}) {
 //     notifcationStream.add(model);
