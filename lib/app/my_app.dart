8c998d59bc8b10d9cf99db5a026b1d29f41f4575
InasAlSaab <sososaab618@gmail.com>
2023-08-11 09:00:31 +0300

    make app arabic

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -3,12 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_templete/core/enums/connectivity_status.dart';
 import 'package:flutter_templete/core/translation/app_translation.dart';
 import 'package:flutter_templete/core/utils/general_utils.dart';
+import 'package:flutter_templete/ui/views/login_view/loginview.dart';
 import 'package:flutter_templete/ui/views/splash_screen/spalsh_screen_view.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get/get_navigation/src/routes/transitions_type.dart';
 import 'package:provider/provider.dart';
-
-import '../ui/views/login_view/loginview.dart';
 
 // ignore: must_be_immutable
 class MyApp extends StatelessWidget {
@@ -18,48 +17,128 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //stream provider +type
-    return MaterialApp(
-
-      debugShowCheckedModeBanner: false,
-      title: 'darrbeni store',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home:LoginView() ,
-    );
-      //StreamProvider<ConnectivityStatus>(
+    return StreamProvider<ConnectivityStatus>(
         //start value onilne
-        //initialData: ConnectivityStatus.ONLINE,
+        initialData: ConnectivityStatus.ONLINE,
         //create stream
-        //create: (context) =>
-        //     //object
-        //     //prodcust app
-        //     //for any thing stream
-        //     connectivityService.connectivityStatusController.stream,
-        //
-        // child: GetMaterialApp(
-         // defaultTransition: Transition.circularReveal,
-        //     transitionDuration: Duration(milliseconds: 12),
-        //     title: 'Flutter Demo',
-        //     builder: BotToastInit(),
-        //     //locale: getlocal(),
-        //     //fallbackLocale: getlocal(),
-        //     translations: AppTranslation(), //1. call BotToastInit
-        //     navigatorObservers: [BotToastNavigatorObserver()],
-        //     debugShowCheckedModeBanner: false,
-        //     theme: ThemeData(
-        //       primarySwatch: Colors.blue,
-        //     ),
-          //  home: SplashSceenView()));
+        create: (context) =>
+            //object
+            //prodcust app
+            //for any thing stream
+            connectivityService.connectivityStatusController.stream,
+        child: GetMaterialApp(
+            defaultTransition: Transition.circularReveal,
+            transitionDuration: Duration(milliseconds: 12),
+            title: 'Flutter Demo',
+            builder: BotToastInit(),
+            locale: getlocal(),
+            fallbackLocale: getlocal(),
+            translations: AppTranslation(), //1. call BotToastInit
+            navigatorObservers: [BotToastNavigatorObserver()],
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: LoginView()));
   }
 }
-//
-// Locale getlocal() {
-//   if (storage.getAppLanguage() == 'ar') {
-//     return Locale('ar', 'SA');
-//   } else if (storage.getAppLanguage() == 'tr') {
-//     return Locale('tr', 'TR');
-//   } else {
-//     return Locale('en', 'US');
-//   }
-// }
+
+Locale getlocal() {
+  if (storage.getAppLanguage() == 'ar') {
+    return Locale('ar', 'SA');
+  } else if (storage.getAppLanguage() == 'tr') {
+    return Locale('tr', 'TR');
+  } else {
+    return Locale('en', 'US');
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+// class RootScaffold {
+//   static openDrawer(BuildContext context) {
+//     final ScaffoldState scaffoldState =
+//         context.rootAncestorStateOfType(TypeMatcher<ScaffoldState>());
+//   }}
