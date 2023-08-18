1140533adff0cba5a4ef0bdfb8b2fc7ed5d226b4
InasAlSaab <sososaab618@gmail.com>
2023-08-18 13:52:21 +0300

    ...

---
+++ lib/ui/views/main_view/important_question/importantQuestion.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ImportantQuestion extends StatefulWidget {
+  const ImportantQuestion({super.key});
+
+  @override
+  State<ImportantQuestion> createState() => _ImportantQuestionState();
+}
+
+class _ImportantQuestionState extends State<ImportantQuestion> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Important"),
+    );
+  }
+}
