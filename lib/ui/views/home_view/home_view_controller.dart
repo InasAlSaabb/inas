7cb4a5205b91f6031c9eecc99a301d98639d4e8f
InasAlSaab <sososaab618@gmail.com>
2023-08-18 15:51:31 +0300

    ..

--- lib/ui/views/home_view/home_view_controller.dart
+++ lib/ui/views/home_view/home_view_controller.dart
@@ -1,1 +1,5 @@
+import 'package:flutter_templete/core/data/models/apis/classmodel.dart';
+import 'package:flutter_templete/core/data/models/apis/slidermodel.dart';
+import 'package:flutter_templete/core/services/base_controller.dart';
 
+class HomeController extends BaseController {}
